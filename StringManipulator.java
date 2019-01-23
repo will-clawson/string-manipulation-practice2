14b745714e98a1f0f31b17850c3d5278593416d6
will-clawson <clawsow@wwu.edu>
2019-01-23 13:00:13 -0800

    got the skeleton working

---
+++ StringManipulator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class StringManipulator{
+
+	private static String ps = "111";
+
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		String attempt = "";
+
+		while(!attempt.equals(ps)){
+
+			System.out.println("What is your password?");
+			attempt = sc.nextLine();
+
+		}
+
+	}
+
+
+
+}
