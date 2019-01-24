6eb3ae213c1bfa0e25317ee46009652a840ef016
will-clawson <clawsow@wwu.edu>
2019-01-24 09:30:24 -0800

    all done!

--- StringManipulator.java
+++ StringManipulator.java
@@ -9,12 +9,28 @@ class StringManipulator{
 		Scanner sc = new Scanner(System.in);
 		String attempt = "";
 
-		while(!attempt.equals(ps)){
+		while(true){
 
-			System.out.println("What is your password?");
+			System.out.print("What is your password? ");
 			attempt = sc.nextLine();
 
+			if(attempt.equals(ps)){
+			System.out.println("Welcome, user.");
+			System.exit(0);
+			}
+
+			else if(attempt.equals("exit")){
+			System.exit(0);
+			}
+
+			else{
+			System.out.println("Wrong password.");
+			}
+
 		}
+
+
+		
 
 	}
 
